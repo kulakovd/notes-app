2c32f17f1122e1ea549cc3b3e9ccd14a78dc7667
Kulakov Dmitry <dmitry.kulakov.00@gmail.com>
2022-01-07 03:26:40 +0500

    Remove redundant class names from App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,8 +4,8 @@ import { Workspace } from '../Workspace';
 
 export const App: React.FC = () => {
   return (
-    <div className="flex items-stretch h-screen bg-white text-black dark:bg-neutral-800 dark:text-white">
-      <div className="absolute inset-0 w-screen md:w-3/12 lg:w-3/12 xl:w-3/12 2xl:w-3/12">
+    <div className="h-screen bg-white text-black dark:bg-neutral-800 dark:text-white">
+      <div className="absolute inset-0 w-screen md:w-3/12">
         <NotesList />
       </div>
       <Workspace />
