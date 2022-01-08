e95795e7a1e08bef465c49f0db148d488cf0dae7
Kulakov Dmitry <dmitry.kulakov.00@gmail.com>
2022-01-08 09:41:16 +0500

    Change focused button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({ icon, text, onClick }) => {
   return (
     <button
-     className="p-1 flex gap-1 items-center text-yellow-600 dark:text-yellow-400 font-semibold hover:text-yellow-500 dark:hover:text-yellow-300 active:opacity-50 transition-all select-none"
+     className="p-1 flex gap-1 items-center text-yellow-600 dark:text-yellow-400 font-semibold hover:text-yellow-500 dark:hover:text-yellow-300 active:opacity-50 transition-all select-none rounded-sm focus:outline focus:outline-2 focus:outline-yellow-300 dark:focus:outline-yellow-600"
      onClick={onClick}
     >
       {icon && (<div className="text-2xl">{icon}</div>)}
